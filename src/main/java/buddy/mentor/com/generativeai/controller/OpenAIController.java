41304b94c8f0c9e58e57906ab902b5dd03386305
Olivier KWIZERA <olivier.kwizera@emeal.nttdata.com>
2024-03-23 16:33:26 +0100

    Changes implementaion, add HTML client, removed unnecessary DTO class

--- src/main/java/buddy/mentor/com/generativeai/controller/OpenAIController.java
+++ src/main/java/buddy/mentor/com/generativeai/controller/OpenAIController.java
@@ -1,10 +1,7 @@
 package buddy.mentor.com.generativeai.controller;
 
-import buddy.mentor.com.generativeai.dto.bookDetails;
 import buddy.mentor.com.generativeai.service.OpenAIService;
-import org.springframework.ai.chat.ChatResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
